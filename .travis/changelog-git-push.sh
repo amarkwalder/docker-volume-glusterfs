aa860fa28988d2599542c7544efe1c8990a06ec5
André Markwalder <andre.markwalder@gmail.com>
2018-06-19 01:20:33 +0200

    Add CHANGELOG.md to repo

--- .travis/changelog-git-push.sh
+++ .travis/changelog-git-push.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-changelog-git-push() {
-    echo "*************** HELLO **************"
-    cat CHANGELOG.md
+setup_git() {
+    git config --global user.email "andre.markwalder@gmail.com"
+    git config --global user.name "amarkwalder"
+    git config --global push.default matching
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 }
 
+make_version() {
+    git add CHANGELOG.md
+    git commit -m "Generate CHANGELOG.md [skip ci]"
+    git push
+}
 
+upload_files() {
+    git push origin HEAD:$TRAVIS_BRANCH
+    git push --tags
+}
+
+changelog-git-push() {
+    setup_git
+    make_version
+    upload_files
+}
